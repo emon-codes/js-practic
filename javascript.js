011a2a738b0fa542f204b9e5e8a056f130fd358c
emon-codes <emon9996a@gmail.com>
2025-10-06 01:37:33 +0600

    add

--- javascript.js
+++ javascript.js
@@ -1,3 +1,50 @@
+// for (count= 1; count<=5; count+1 ){
+//     console.log("apna collage")
+// }
+
+// for (count = 1; count <= 500; count++) {
+//     console.log("apna collage");
+// }
+
+// for(let count = 1; count <= 10; count++){{
+//     console.log(count);
+// }
+// for(let i= 1; i <= 20; i++){
+// console.log("2 x "+ i +"=" +(2*i));
+
+// }
+
+// for(let k= 1; k<= 50; k++){
+// console.log("2 x" + k+ "=" + (k*2));
+// }
+
+// for(let a=10; a>=1; a--){
+// console.log(a)
+
+// }
+
+// for (let i = 5; i >= 1; i--) {
+//    console.log(i);
+// }
+
+// for (let i = 1; i <= 5000; i++) {
+//    console.log("Apna College" +i);
+// }
+
+// for(d = 1; d <=100; d++){
+//     console.log(d)
+// }
+
+// let gameNumber = 10;
+
+// let userNum = prompt("Guess the game number:");
+
+// while ( userNum != gameNumber){
+// userNum= prompt("inter right number")
+// };
+// console.log("congration u are right")
+
+///////////////////////////////////////////
 // function sefat() {
 //   console.log("welcome");
 // }

a1d79f2d6a6f7b6a30d4b6596a8e59719dec1c39
emon-codes <emon9996a@gmail.com>
2025-10-07 04:09:24 +0600

    add item

--- javascript.js
+++ javascript.js
@@ -73,29 +73,65 @@
 
 // higherOrderFunction(6, squre);
 
-const taskOne = () => {
-  console.log("task1");
-};
+// const taskOne = () => {
+//   console.log("task1");
+// };
 
-const dataLoading = () => {
-  console.log("task2. Data loading....");
-};
+// const dataLoading = () => {
+//   console.log("task2. Data loading....");
+// };
 
-const taskTwo = () => {
-  setTimeout(dataLoading, 2000);
-};
-const taskThree = () => {
-  console.log("task3");
-};
-const taskFour = () => {
-  console.log("task4");
-};
-const taskFive = () => {
-  console.log("task5");
-};
+// const taskTwo = () => {
+//   setTimeout(dataLoading, 2000);
+// };
+// const taskThree = () => {
+//   console.log("task3");
+// };
+// const taskFour = () => {
+//   console.log("task4");
+// };
+// const taskFive = () => {
+//   console.log("task5");
+// };
 
-taskOne();
-taskTwo();
-taskThree();
-taskFour();
-taskFive();
+// taskOne();
+// taskTwo();
+// taskThree();
+// taskFour();
+// taskFive();
+
+///////////////////////////////////////////////////////
+
+// let students = ["A", "B", "C"];
+
+// students.forEach((student, idx) => {});
+
+// let all = students.map((std, idx) => {});
+
+// let studentName = ["emon", "rakib", "talha", "sahin", "sefat", "sayem"];
+
+// studentName.forEach((student, index) => {
+//   console.log(`this is student name ${student} this is index num ${index}`);
+// });
+
+// let studentId = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+// studentId.forEach((student, idx, array) => {
+//   console.log(`${student}.idx num ${idx} array ${array} `);
+// });
+
+// const vl = [1, 2, 3, 4, 5];
+
+// vl.forEach((x, y) => {
+//   console.log(`${x} : ${y}`);
+// });
+
+// let count = [1, 2, 3, 4, 5];
+
+// let dubole = count.map((m) => m * 2);      // er khanea (m) holo perameter
+// console.log(dubole);
+
+const vl = [2, 3, 6, 4, 5, 7, 8];
+
+const duble = vl.map((m) => m * 2);
+console.log(duble);

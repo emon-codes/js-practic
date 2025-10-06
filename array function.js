a631f9e1ec968d6673674f17c74716311b5b52f7
emon-codes <emon9996a@gmail.com>
2025-10-07 04:16:48 +0600

    array function add

---
+++ array function.js
@@ -0,0 +1,33 @@
+// let students = ["A", "B", "C"];
+
+// students.forEach((student, idx) => {});
+
+// let all = students.map((std, idx) => {});
+
+// let studentName = ["emon", "rakib", "talha", "sahin", "sefat", "sayem"];
+
+// studentName.forEach((student, index) => {
+//   console.log(`this is student name ${student} this is index num ${index}`);
+// });
+
+// let studentId = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+// studentId.forEach((student, idx, array) => {
+//   console.log(`${student}.idx num ${idx} array ${array} `);
+// });
+
+// const vl = [1, 2, 3, 4, 5];
+
+// vl.forEach((x, y) => {
+//   console.log(`${x} : ${y}`);
+// });
+
+// let count = [1, 2, 3, 4, 5];
+
+// let dubole = count.map((m) => m * 2);      // er khanea (m) holo perameter
+// console.log(dubole);
+
+const vl = [2, 3, 6, 4, 5, 7, 8];
+
+const duble = vl.map((m) => m * 2);
+console.log(duble);

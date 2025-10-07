44583e9c093ed5673d1cd16fc1bca55c1d16645d
emon-codes <emon9996a@gmail.com>
2025-10-08 02:58:29 +0600

    array method

---
+++ array method.js
@@ -0,0 +1,31 @@
+// let roll = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// let riting = roll.filter((num) => num > 2);
+// console.log(riting);
+
+// let studentName = ["Rakib", "roman", "lsl", "sayem", "talha"];
+
+// studentName.splice(1, 2, "roki", "rony");
+
+// console.log(studentName);
+
+// let studentName = ["Rakib", "roman", "lsl", "sayem", "talha"];
+// studentName.push("roki", " rony", " jahid");
+// console.log(studentName);
+
+
+// let studentName = ["Rakib", "roman", "lsl", "sayem", "talha"];
+// studentName.pop();
+// console.log(studentName);
+
+
+
+// let studentName = ["Rakib", "roman", "lsl", "sayem", "talha"];
+// studentName.shift();
+// console.log(studentName);
+
+
+
+// let Name = ["Rakib", "roman", "lsl", "sayem", "talha"];
+// fruits.unshift("aple", "khan", "sahin");
+// console.log(Name);

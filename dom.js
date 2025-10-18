80c4aa094004b9e09416a01dacdd8ab03380da09
emon ahmed rana <emon9996a@gmail.com>
2025-10-18 20:48:00 +0600

    dom js state

---
+++ dom.js
@@ -0,0 +1,28 @@
+
+// function changeName(){
+//     document.getElementById("name").innerText= "wellcom Shifat"
+// }
+
+
+    //   const changeText = document.querySelector("#kek");
+      
+    //   changeText.innerText = "bangladesh";
+
+
+
+// const colorChange = document.querySelectorAll(".lorem1");
+// colorChange.forEach((p) => {
+//     p.style.color = "red";
+// })
+
+
+// const fontChange = document.querySelector
+
+// function click() {
+//     document.querySelector(".lorem1").innerText = "paragraph";
+// }
+
+    function click() {
+      document.querySelector(".lorem1").innerText = "paragraph";
+    }
+

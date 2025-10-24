3ad71eda3352e3b2265c984b822616f73e870b9f
EMON AHMED RANA <emon9996a@gmail.com>
2025-10-25 03:16:12 +0600

    fetch js

---
+++ getpost.js
@@ -0,0 +1,48 @@
+// const prent = document.getElementById("prent");
+
+// const fragment = document.createDocumentFragment()
+
+
+
+
+// async function getData() {
+    
+// try{
+
+// const resorch = await fetch("https://jsonplaceholder.typicode.com/users");
+// const posts = await resorch.json()
+
+// posts.forEach(post => {
+
+// const div = document.createElement("ul");
+
+// div.className= "container";
+
+// div.innerHTML = `id ${post.id} username ${post.username} Email ${post.email}`
+
+// fragment.appendChild(div)
+
+// })
+
+// prent.appendChild(fragment)
+
+// }
+
+// catch(error){
+//     console.error(error)
+// }
+
+// finally{
+//     console.log("work done")
+// }
+
+
+// }
+
+// getData()
+
+
+
+
+
+

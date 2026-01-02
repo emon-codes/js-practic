10f4778f1d9d65a94c70d2ea7c09cb65a62aa2b2
EMON AHMED RANA <emon9996a@gmail.com>
2026-01-02 20:03:12 +0600

    Conditional Statement pracktis

--- Script.js
+++ Script.js
@@ -1,5 +1,12 @@
 var age = 30;
+let jela = "mymensingh"
+let country = "banglades"
 
-if (age >= 18) {
-    console.log("you can bote")
+if (age >= 18 && country === "bangladesh" && jela === "mymensingh") {
+    console.log("you can vote")
 }
+else {
+    console.log("you can not vote")
+}
+
+

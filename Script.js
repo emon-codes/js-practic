e5d04f487c84a11f9e2bfb2b75873fdf7421d09e
EMON AHMED RANA <emon9996a@gmail.com>
2026-01-03 23:24:25 +0600

    tarnary oprator

--- Script.js
+++ Script.js
@@ -1,12 +1,29 @@
-var age = 30;
-let jela = "mymensingh"
-let country = "banglades"
+// var age = 30;
+// let jela = "mymensingh"
+// let country = "bangladesh"
+// let isCityzin = true;
 
-if (age >= 18 && country === "bangladesh" && jela === "mymensingh") {
-    console.log("you can vote")
-}
-else {
-    console.log("you can not vote")
-}
+// if (age >= 18 && country === "bangladesh" && jela === "mymensingh" && isCityzin) {
+//     console.log("you can vote")
+// }
+// else {
+//     console.log("you can not vote")
+// }
 
 
+// // tarnary oprator 
+
+// let max = (20 > 20) ? "right" : "rong"
+// console.log(max)
+
+
+
+let age = 22;
+let country = "bangladesh";
+
+let message =
+  age >= 18 && country === "bangladesh"
+    ? "You are eligible to vote"
+    : "You are not eligible to vote";
+
+console.log(message);

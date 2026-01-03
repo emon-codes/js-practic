691561f0bc5746db20b7a4ff43b4c764f6beb97b
EMON AHMED RANA <emon9996a@gmail.com>
2026-01-04 00:09:03 +0600

    loop use place

--- loop.js
+++ loop.js
@@ -19,3 +19,11 @@ do {
     console.log(counts);
     counts++;
 } while (count <= 5);
+
+
+
+// for loop ব্যবহার করা হয় যখন iteration সংখ্যা জানা থাকে
+
+// while loop ব্যবহার করা হয় যখন iteration সংখ্যা জানা থাকে না
+
+// do...while loop ব্যবহার করা হয় যখন কমপক্ষে একবার execute করতেই হয়

a5b2f1b19fa3ba8397ed04a7584de8ad27a87d86
EMON AHMED RANA <emon9996a@gmail.com>
2026-01-04 22:04:01 +0600

    array function example

--- function.js
+++ function.js
@@ -23,4 +23,23 @@ function multiplay(value1, value2) {
     console.log(value1 * value2)
 }
 
-multiplay(5, 6);
+multiplay(5, 6);        //ei khanea "5, 6" holo argument
+
+
+
+
+
+
+// array function
+
+
+const squere = () => {
+    console.log("this is array function")
+}
+squere()
+
+
+const plass = (x, y) => {
+    console.log(x*y)
+}
+plass(20, 2);

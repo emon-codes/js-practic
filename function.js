8ae89509af01a5c095ad25693ca8481d14232e3c
EMON AHMED RANA <emon9996a@gmail.com>
2026-01-04 21:54:48 +0600

    function part

---
+++ function.js
@@ -0,0 +1,26 @@
+function coll() {
+    console.log("welcome to function part")
+}
+
+coll();
+
+
+// Anonymus function
+
+
+const hello = function () {
+    console.log("Anonymus function example")
+}
+hello()
+
+
+
+// function with paramiter
+
+// "value1, value2" ei gula hoila paramiter
+
+function multiplay(value1, value2) {
+    console.log(value1 * value2)
+}
+
+multiplay(5, 6);

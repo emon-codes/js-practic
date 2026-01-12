a4143ee47f1d4b4bc6f7ae6c39c8aa8504f4cfb9
EMON AHMED RANA <emon9996a@gmail.com>
2026-01-12 20:47:03 +0600

    akadik condation tarnary oprator use

--- tarnary-oprator.js
+++ tarnary-oprator.js
@@ -1,3 +1,13 @@
-let number = Number(prompt("inter your number"));
+// let number = Number(prompt("inter your number"));
 
-number > 0 ? console.log("projative") : console.log("nagative")
+// number > 0 ? console.log("projative") : console.log("nagative")
+
+
+
+
+let roll = Number(prompt("inter your Roll"))
+
+let result = roll === 1 ? "A+" : roll == 2 ? "B" : roll == 3 ? "C" : roll == 4 ? "D" : "F";
+
+console.log(result);
+

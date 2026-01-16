7518e1b28c973fd991f9c9ea3f90078617eafd50
EMON AHMED RANA <emon9996a@gmail.com>
2026-01-16 21:41:46 +0600

    dom

--- dom.js
+++ dom.js
@@ -29,20 +29,3 @@
     // }
 
 
-    const items = document.getElementById("items");
-
-    const fragment = document.createDocumentFragment();
-
-
-
-    for (let i= 0 ; i <= 100 ; i++){
-      const li = document.createElement("li");
-
-      li.textContent ="items" + i;
-
-      fragment.appendChild(li);
-
-
-    }
-
-    items.appendChild(fragment);
